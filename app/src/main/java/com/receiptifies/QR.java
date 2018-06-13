2ab72befdf50bbe1b36709d2220d5c1a98598ce8
alvarezaaronai <alvarez.a.aaron@gmail.com>
2018-06-13 14:15:49 -0700

    added a new interface

---
+++ app/src/main/java/com/receiptifies/QR.java
@@ -0,0 +1,13 @@
+package com.receiptifies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+    }
+}
