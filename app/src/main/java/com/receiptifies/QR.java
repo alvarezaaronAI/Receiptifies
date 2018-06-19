6582ef36acb021d527dfe3fb2435b09d8fc5caf7
alvarezaaronai <alvarez.a.aaron@gmail.com>
2018-06-19 15:21:41 -0700

    modified the path to pictures

--- app/src/main/java/com/receiptifies/QR.java
+++ app/src/main/java/com/receiptifies/QR.java
@@ -1,23 +1,23 @@
 package com.receiptifies;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 
 import java.io.File;
 import java.io.IOException;
@@ -155,7 +155,7 @@ public class QR extends AppCompatActivity {
         return image;
     }
     //end createImageFile.
-
+    
 /*
 //This code will only work with API MIN Level 23 and greater.
 
