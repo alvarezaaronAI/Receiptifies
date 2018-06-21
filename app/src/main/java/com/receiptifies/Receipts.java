49b5e2aab9796c236014aca8bf0f0f5cc9327ac6
alvarezaaronai <alvarez.a.aaron@gmail.com>
2018-06-20 19:19:51 -0700

    added new button to go to receipts activity

---
+++ app/src/main/java/com/receiptifies/Receipts.java
@@ -0,0 +1,13 @@
+package com.receiptifies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Receipts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipts);
+    }
+}
