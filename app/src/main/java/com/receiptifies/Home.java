f3ce1df96bfc120fb8b36f9e9cacdda8e2ee1e9f
alvarezaaronai <alvarez.a.aaron@gmail.com>
2018-06-21 13:53:50 -0700

    changed home interface to satisfy, cleaned home activity onclick handler.

--- app/src/main/java/com/receiptifies/Home.java
+++ app/src/main/java/com/receiptifies/Home.java
@@ -17,20 +17,26 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "You are now in QR Scanning ", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                //creating action to go to QR Scanning
-                Intent intent = new Intent(Home.this, QR.class);
-                startActivity(intent);
-
-            }
-        });
     }
     //end onCreate
-    
+
+    //Method that handles camera in home activity.
+    public void cameraPreview(View view) {
+        Snackbar.make(view, "You are now in QR Scanning ", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show();
+        //creating action to go to QR Scanning
+        Intent intent = new Intent(Home.this, QR.class);
+        startActivity(intent);
+    }
+    //end of cameraPreview.
+
+    //Method that will handle receipts in home activity.
+    public void receiptsPreview(View view) {
+        Snackbar.make(view, "You are now in Receipts Page", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+        //creating action to go to QR Scanning
+        Intent intent = new Intent(Home.this, Receipts.class);
+        startActivity(intent);
+    }
+    //end receiptsPreview.
 }
